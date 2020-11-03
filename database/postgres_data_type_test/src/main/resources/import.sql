1525303580ffd68a29c85e2b3990394877a296d2
jongseong woo <rura6502@hotmail.com>
2020-11-03 15:54:02 +0900

    fix: move database snippet codes to project java snippets
     * this code is just for data type converting test between postgres and
       java, not related spring. so move this project to repository for java
       snippets

--- database/postgres_data_type_test/src/main/resources/import.sql
+++
@@ -1,9 +0,0 @@
-
-
--- for jsonb test
-CREATE TABLE public.tbl_test_json (
-	id varchar NOT NULL,
-	test_jsonb jsonb NULL,
-  test_json json NULL,
-	CONSTRAINT tbl_process_model_request_pk PRIMARY KEY (id)
-);
